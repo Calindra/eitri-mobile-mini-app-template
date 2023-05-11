717ef3550eb978ea7e1b3318e38c6c1cb772fe95
Gabriel Fonseca <3rd.gabriel.silva@amedigital.com>
2023-05-11 10:55:27 -0300

    fix: miniapp conf

--- miniapp.conf.js
+++ miniapp.conf.js
@@ -1,16 +1,47 @@
 module.exports = {
-    "name": "up-template",
-    "title": "up-template",
-    "slug": "up-template",
+    /**
+     * Nome do seu mini app
+     * O nome será utilizado para identificar o seu mini-app dos demais já existentes.
+     */
+    "name": "Meu Mini App",
+    /**
+     * Esta propriedade define o título do mini-app na barra de título do aplicativo
+     */
+    "title": "Meu super mini app",
+    /**
+     * Utilize aqui um identificador único que seja de fácil leitura.
+     * Não utilize espaços em branco.
+     * Sugerimos utilizar um domínio reverso como slug.
+     */
+    "slug": "br.com.minhaempresa.vendas",
+    /**
+     * Aqui você pode incluir a versão do seu mini-app.
+     * Lembre-se que você só poderá enviar 1 versão por vez para publicação.
+     */
+    "version": "0.1.0",
+    /**
+     * Insira os dados da sua empresa,  (Nome da empresa pode conter espaços)
+     * Não esqueça também do seu email de propriedade do mini-app.
+     */
     "organization": {
-        "name": "up-template",
-        "owner": {
-            "email": "gabriel.silva@calindra.com.br"
-        }
+      "name": "Minha Empresa",
+      "owner": {
+        "email": "seuemail@suaempresa.com"
+      },
     },
-    "public-key": "0e0eef2a-bc50-4a63-88ce-7dcfafc3678d",
-    "ame-super-app-client": "2.27.0",
-    "ame-miniapp-components": "2.24.1",
-    "undefined": "0.1.0",
-    "version": "0.1.0"
-}
+    /**
+     * Aqui nesta linha você determina a versão da biblioteca de componentes que você irá utilizar.
+     * Mais informações em: https://ame-miniapp-components.calindra.com.br/docs/estrutura/
+     */
+    "ame-miniapp-components": "2.6.1",
+    /**
+     * Aqui nesta linha você determina a versão do super-app-client que você irá utilizar.
+     * Mais informações em: https://super-app-client.calindra.com.br/docs/ame-super-app-client/
+     */
+    "ame-super-app-client": "2.9.0",
+    /**
+     * Aqui nesta linha você determina a versão do sdk-support que você irá utilizar.
+     * Mais informações em: https://ame-sdk-support.calindra.com.br/
+     */
+    "ame-mini-app-sdk-support": "1.0.0",
+  }
