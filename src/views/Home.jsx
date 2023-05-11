a4eee127561ba3649dbeba078889cc7d23e847bc
garrydias <garrydias@gmail.com>
2023-05-11 18:47:14 -0300

    Update Home.jsx
    Remocao de referencias ao repo base

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,6 @@
 <Window>
 	<View>
-    <Illustration height={200} image={require('../assets/images/icon_ame.svg')}/>
+    <Illustration height={200} image={require('../assets/images/icon_eitri.svg')}/>
     <Header textAlign="center">Boilerplate Mini App</Header>
     <Spacing size="lg" />
     <ListView items={this.state.links} onItemSelect={this.navigateTo} />
