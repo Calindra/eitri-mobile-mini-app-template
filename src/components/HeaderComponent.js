773530d56477cd6dbd1e85cc19068a8f63b785dc
Madeira <marcelo.madeira@calindra.com.br>
2023-05-12 11:26:20 -0300

    feat: :bug: Removendo detalhes

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,3 @@
-import basketWhite from '../assets/images/basket-white.svg';
-
 export default class HeaderComponent {
     
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,3 @@
-import Ame from "ame-super-app-client";
-
 export default class Home {
 
   async componentDidMount() {
