6d1a910bc1920854fe1f081228392d8effee7ca6
Madeira <marcelo.madeira@calindra.com.br>
2023-06-27 15:40:43 -0300

    Atualizando dependências

--- miniapp.conf.js
+++ miniapp.conf.js
@@ -1,46 +1,48 @@
 module.exports = {
-    /**
-     * Nome do seu mini app
-     * O nome será utilizado para identificar o seu mini-app dos demais já existentes.
-     */
-    "name": "Meu Mini App",
-    /**
-     * Esta propriedade define o título do mini-app na barra de título do aplicativo
-     */
-    "title": "Meu super mini app",
-    /**
-     * Utilize aqui um identificador único que seja de fácil leitura.
-     * Não utilize espaços em branco.
-     * Sugerimos utilizar um domínio reverso como slug.
-     */
-    "slug": "br.com.minhaempresa.vendas",
-    /**
-     * Aqui você pode incluir a versão do seu mini-app.
-     * Lembre-se que você só poderá enviar 1 versão por vez para publicação.
-     */
-    "version": "0.1.0",
-    /**
-     * Insira os dados da sua empresa,  (Nome da empresa pode conter espaços)
-     * Não esqueça também do seu email de propriedade do mini-app.
-     */
-    "organization": {
-      "name": "Minha Empresa",
-      "owner": {
-        "email": "seuemail@suaempresa.com"
-      },
+  /**
+   * Nome do seu mini app
+   * O nome será utilizado para identificar o seu mini-app dos demais já existentes.
+   */
+  "name": "Meu Mini App",
+  /**
+   * Esta propriedade define o título do mini-app na barra de título do aplicativo
+   */
+  "title": "Meu super mini app",
+  /**
+   * Utilize aqui um identificador único que seja de fácil leitura.
+   * Não utilize espaços em branco.
+   * Sugerimos utilizar um domínio reverso como slug.
+   */
+  "slug": "br.com.minhaempresa.vendas",
+  /**
+   * Aqui você pode incluir a versão do seu mini-app.
+   * Lembre-se que você só poderá enviar 1 versão por vez para publicação.
+   */
+  "version": "0.1.0",
+  /**
+   * Insira os dados da sua empresa,  (Nome da empresa pode conter espaços)
+   * Não esqueça também do seu email de propriedade do mini-app.
+   */
+  "organization": {
+    "name": "Minha Empresa",
+    "owner": {
+      "email": "seuemail@suaempresa.com"
     },
-    /**
-     * Aqui nesta linha você determina a versão da biblioteca de componentes que você irá utilizar.
-     */    
-    "eitri-app-components": "",
-        
-    /**
-     * Aqui nesta linha você determina a versão do super-app-client que você irá utilizar.
+  },
+  /**
+   * Aqui nesta linha você determina a versão da biblioteca de componentes que você irá utilizar.
+   */
+  "eitri-app-components": "0.7.14",
+  /**
+   * Aqui nesta linha você determina a versão do super-app-client que você irá utilizar.
+   */
+  "eitri-app-client": "0.16.0",
+  /**
+     * Aqui nesta linha você determina a versão da biblioteca comum que você irá utilizar.
      */
-    "eitri-app-client": "https://eitri.s3.amazonaws.com/eitri-app-client-0.8.0.tgz",
-    
-    /**
-     * Aqui nesta linha você determina a versão do sdk-support que você irá utilizar.
-     */
-    //"eitri-app-sdk-support": "1.0.0",
-  }
+  "eitri-commons": "0.0.19",
+  /**
+   * Aqui nesta linha você determina a versão do sdk-support que você irá utilizar.
+   */
+  //"eitri-app-sdk-support": "1.0.0",
+}
