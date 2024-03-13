2e7432e535be8ad31e3b61fb4f3f379ef8db22de
Gabriel Fonseca <gabriel.silva@calindra.com.br>
2024-03-13 10:31:36 -0300

    refactor: local assets import

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -2,7 +2,7 @@
     <View topInset />
     <View direction='row' justifyContent='start' alignItems='center' padding='nano' minHeight={60} backgroundColor="neutral-900">
         <Touchable width={50} onPress={this.onBackClick}>
-            <Image src={require('../assets/images/arrow_left.png')} cover width={40} />
+            <Image src={'assets/images/arrow_left.png'} cover width={40} />
         </Touchable>
         <Text wide textAlign='left' color='neutral-100' fontWeight='bold' fontSize='small'>Meet Eitri!</Text>
     </View>
